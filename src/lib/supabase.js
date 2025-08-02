9e9c992009724310d850759549c9dbfff028563c
kinaar8340 <puf_puf_pass0@proton.me>
2025-08-02 14:16:11 -0700

    Add debug log for Supabase key

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,5 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 console.log('Supabase Anon Key:', process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY)
+console.log('Supabase URL:', process.env.NEXT_PUBLIC_SUPABASE_URL);
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
