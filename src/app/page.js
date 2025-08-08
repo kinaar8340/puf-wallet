d5edcb88316572d8fbfbff8e5e965297acd6de88
kinaar8340 <puf_puf_pass0@proton.me>
2025-08-07 17:18:39 -0700

    edited a description for export metadata

--- src/app/page.js
+++ src/app/page.js
@@ -415,7 +415,7 @@ export default function Home() {
         )}
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        {/* Footer*/}
+        {/* Footer ya or nay*/}
       </footer>
       <ToastContainer theme="dark" />
     </div>
