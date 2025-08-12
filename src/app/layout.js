00764cb6d6cf14063306a8e2ff58060f46ab3a46
kinaar8340 <puf_puf_pass0@proton.me>
2025-08-11 20:34:15 -0700

    removed dead links

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,10 +14,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png">
-        <link rel="manifest" href="/images/site.webmanifest"></link>
         <link rel="icon" href="/images/icon0.png" type="image/png" />
       </head>
       <body className="bg-[url('/images/bg1.png')] bg-fixed bg-cover bg-center">
