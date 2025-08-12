45741ac1b24cfb23130a76a89cb8613728b3edf6
kinaar8340 <puf_puf_pass0@proton.me>
2025-08-12 14:59:52 -0700

    added icon to browser tab

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import './globals.css';
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en"> // className="dark"
+    <html lang="en" className="dark"> 
       <head>
         <link rel="icon" href="/images/icon0.png" type="image/png" />
       </head>
