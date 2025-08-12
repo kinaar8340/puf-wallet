aa05ef30062adb526268c26e85936374610b854f
kinaar8340 <puf_puf_pass0@proton.me>
2025-08-12 16:45:37 -0700

    testing coded

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,7 +8,7 @@ export default function RootLayout({ children }) {
         <link rel="icon" href="/images/icon0.png" type="image/png" />
       </head>
       <body className="font-sans min-h-screen relative bg-[url('/images/bg1.png')] bg-cover bg-center bg-no-repeat">
-        <div className="absolute inset-0 bg-black opacity-50 z-0"></div>
+        //<div className="absolute inset-0 bg-black opacity-50 z-0"></div>
         <main className="relative z-10">
           <Providers>
             {children}
