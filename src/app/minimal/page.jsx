6fd3699b8cef7304df43c4564ffaa7e81033ad69
kinaar8340 <puf_puf_pass0@proton.me>
2025-08-13 00:28:12 -0700

    test

--- src/app/minimal/page.jsx
+++ src/app/minimal/page.jsx
@@ -42,12 +42,10 @@ return (
       {/* Combined container for header and controls on the same background */}
       <div className="w-full rounded-lg shadow-md shadow-green-500/50 mt-8 bg-black/50">
         {/* Header section */}
-        <div className="w-full p-4 flex justify-between items-center">
-          <img src="/images/icon0.png" alt="PUF Wallet" className="w-32 h-32 py-3 px-5 rounded object-contain object-top object-left" />
-          <div className="flex flex-col items-end gap-4">
-            <WalletMultiButton className="font-bold py-3 px-5 rounded text-xl bg-gradient-to-br from-blue-500/70 to-blue-600/70 hover:bg-blue-600/70" />
-            {publicKey && <p className="text-lg font-bold text-right mx-auto text-[#00ff00]">Connected: {publicKey.toBase58().slice(0, 6)}...{publicKey.toBase58().slice(-4)}</p>}
-          </div>
+        <div className="w-full p-5 flex justify-top items-center">
+          <img src="/images/icon0.png" alt="PUF Wallet" className="w-32 h-32 object-contain object-top object-left" />
+          <WalletMultiButton className="font-bold py-3 px-5 rounded text-xl bg-gradient-to-br from-blue-500/70 to-blue-600/70 hover:bg-blue-600/70" />
+          {publicKey && <p className="text-lg font-bold text-right mx-auto text-[#00ff00]">Connected: {publicKey.toBase58().slice(0, 6)}...{publicKey.toBase58().slice(-4)}</p>}
         </div>
         <p className="bg-black/1 p-5 text-lg font-bold text-right mx-auto text-[#00ff00]">| $PUF: {Number(balance).toFixed(2)}  |  </p>
           <div className="w-full bg-black/75 p-5 rounded-b-lg shadow-md shadow-green-500/50 text-[#00ff00]">
