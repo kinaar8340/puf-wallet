da4f6a940cc3fdd76a0b44857fc29488616079fb
kinaar8340 <puf_puf_pass0@proton.me>
2025-08-12 17:15:34 -0700

    fixed syntax error random char

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,19 @@
+// ~/puf-wallet-frontend/src/app/layout.js
+
 import { Providers } from './Providers';
 import './globals.css';
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="dark"> 
+    <html lang="en" className="dark">
       <head>
-        <link rel="icon" href="/images/icon0.png" type="image/png" />
+        <link rel="icon" href="/images/icon0.png" type="image/png"/>
       </head>
       <body className="font-sans min-h-screen relative bg-[url('/images/bg1.png')] bg-cover bg-center bg-no-repeat">
-        /* <div className="absolute inset-0 bg-black opacity-50 z-0"></div> */
-        <main className="relative z-10">
-          <Providers>
+        <Providers>
             {children}
-          </Providers>
-        </main>
+        </Providers>
       </body>
     </html>
   );
-} 
+}
