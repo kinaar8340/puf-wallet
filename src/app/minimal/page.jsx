79f0bd9912547833f3d1d1c05057780fcf553867
kinaar8340 <puf_puf_pass0@proton.me>
2025-08-13 00:35:26 -0700

    moved logo

--- src/app/minimal/page.jsx
+++ src/app/minimal/page.jsx
@@ -43,11 +43,11 @@ return (
       <div className="w-full rounded-lg shadow-md shadow-green-500/50 mt-8 bg-black/50">
         {/* Header section */}
         <div className="w-full p-5 flex justify-top items-center">
-          <img src="/images/icon0.png" alt="PUF Wallet" className="w-32 h-32 object-contain object-top object-left" />
           <WalletMultiButton className="font-bold py-3 px-5 rounded text-xl bg-gradient-to-br from-blue-500/70 to-blue-600/70 hover:bg-blue-600/70" />
           {publicKey && <p className="text-lg font-bold text-right mx-auto text-[#00ff00]">Connected: {publicKey.toBase58().slice(0, 6)}...{publicKey.toBase58().slice(-4)}</p>}
         </div>
-        <p className="bg-black/1 p-5 text-lg font-bold text-right mx-auto text-[#00ff00]">| $PUF: {Number(balance).toFixed(2)}  |  </p>
+        <img src="/images/icon0.png" alt="PUF Wallet" className="w-32 h-32 object-contain object-center object-left" />
+        <p className="w=1/2 bg-black/1 p-5 text-lg font-bold text-left mx-auto text-[#00ff00]">| $PUF: {Number(balance).toFixed(2)}  |  </p>
           <div className="w-full bg-black/75 p-5 rounded-b-lg shadow-md shadow-green-500/50 text-[#00ff00]">
             <div className="w-full flex justify-center gap-4 mb-4">
               <Link href="/results">
