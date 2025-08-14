d4f3c5eea8b37909a26c61428e1637ee79e79d27
kinaar8340 <puf_puf_pass0@proton.me>
2025-08-14 02:05:29 -0700

    styling test

--- src/app/minimal/page.tsx
+++ src/app/minimal/page.tsx
@@ -53,7 +53,7 @@ export default function Minimal() {
   <div suppressHydrationWarning={true} className="font-sans grid grid-rows-[1fr_20px] items-start justify-items-center min-h-screen px-4 pb-4 gap-8 sm:px-10 sm:pb-10 text-xl text-[#00ff00] bg-transparent relative">
     <main className="flex flex-col items-center justify-center w-full max-w-2xl mx-auto p-1 sm:p-10">
       {/* Combined container for header and controls on the same background */}
-      <div className="fixed top-0 left-0 w-1/3 mx-auto rounded-lg shadow-md shadow-green-500/50 bg-black/50 z-10">
+      <div className="top-10 justify center w-1/3 mx-auto rounded-lg shadow-md shadow-green-500/50 bg-black/50 z-10">
         {/* Header section */}
           <div className="w-full p-5 flex justify-center items-center">
             <button
@@ -61,7 +61,7 @@ export default function Minimal() {
               className="font-bold py-3 px-5 rounded text-xl bg-gradient-to-br from-blue-500/70 to-blue-600/70 hover:bg-blue-600/70 text-[#00ff00] shadow-md shadow-green-500/50">
               {buttonLabel}
             </button>
-          </div>
+          </div> 
         <div className="w-full p-5 flex justify-between items-center">
           <img src="/images/icon0.png" alt="PUF Wallet" className="w-32 h-32 object-contain object-center" />
           <p className="bg-black/1 text-lg font-bold text-center mx-auto text-[#00ff00]">$PUF: {Number(balance).toFixed(2)}</p>
